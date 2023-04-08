bb234f4b74c33bceb8310b364445423a529b3a32
Konstantin K <kkozhukhov@yandex.ru>
2023-04-08 18:35:23 +0300

    fix flake8 problems

--- main.py
+++ main.py
@@ -8,24 +8,23 @@ offers_and_feeds = []
 ut.get_files_xml()
 
 # Проходим по всем скачанным файлам
-# забираем значения id из каждого тэга offer  
+# забираем значения id из каждого тэга offer
 directory = 'xml_files'
 
 for filename in os.listdir(directory):
-	f = os.path.join(directory, filename)
-	
-	if os.path.isfile(f):
-		ut.get_values_from_xml(f, offers_and_feeds)
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        ut.get_values_from_xml(f, offers_and_feeds)
 
 
 # проверяем есть ли изменения в фидах
 if ut.check_counter(len(offers_and_feeds)):
     print('Генерация promos.xml')
     ut.generate_xml(offers_and_feeds)
-    
+
 else:
     print('Генерация promos.xml не требуется. Остановка скрипта')
     exit()
-    
+
 # Сохраняем в отдельном файле количество offer
 ut.save_offers_counter(len(offers_and_feeds))
